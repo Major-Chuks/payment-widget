5820f09c3414428adef62911baca7b81c82b9f4c
major-chuks <chukaezeka@gmail.com>
2025-12-09 03:20:44 +0100

    update config

--- next.config.ts
+++ next.config.ts
@@ -8,20 +8,22 @@ const nextConfig: NextConfig = {
     "pino-pretty",
     "@walletconnect/universal-provider",
     "@walletconnect/ethereum-provider",
+    "idb-keyval",
+    "unstorage",
   ],
 
-  // Turbopack configuration for Next.js 16+
-  turbopack: {
-    resolveAlias: {
-      pino: "pino/browser.js",
-    },
-  },
-
-  // Fallback webpack config (only used if --webpack flag is passed)
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals = config.externals || [];
-      config.externals.push("pino", "thread-stream", "pino-pretty");
+      config.externals.push(
+        "pino",
+        "thread-stream",
+        "pino-pretty",
+        "@walletconnect/universal-provider",
+        "@walletconnect/ethereum-provider",
+        "idb-keyval",
+        "unstorage"
+      );
     }
     return config;
   },
